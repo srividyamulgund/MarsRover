4bcf6699e07316b0aebb14452baa555ea3808d58
Srividya Mulgund <srividya.mulgund@gmail.com>
2021-05-18 13:02:29 +0100

    Added MotionController Rover Position InBounds more tests

--- src/test/java/com/marsrover/MotionControllerTest.java
+++ src/test/java/com/marsrover/MotionControllerTest.java
@@ -11,11 +11,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class MotionControllerTest {
 
     @Test
-    @DisplayName("returns false if Rover is outside the Plateau")
-    public void testRover1Position() {
+    @DisplayName("returns true if Rover is inside the Plateau")
+    public void testRoverPositionInsidePlateau() {
         Plateau plateau = new Plateau(5, 5);
         Rover rover1 = new Rover(1, 2, 'N');
         MotionController motionControllerRover1 = new MotionController(plateau, rover1);
         Assertions.assertTrue(motionControllerRover1.isRoverPositionInBounds(plateau, rover1), "Rover cannot be out of the Plateau");
+    }
+
+    @Test
+    @DisplayName("returns false if Rover is outside the Plateau")
+    public void testRoverPositionOutsidePlateau() {
+        Plateau plateau = new Plateau(5, 5);
+        Rover rover1 = new Rover(7, 7, 'N');
+        MotionController motionControllerRover1 = new MotionController(plateau, rover1);
+        Assertions.assertFalse(motionControllerRover1.isRoverPositionInBounds(plateau, rover1), "Rover cannot be out of the Plateau");
     }
 }
