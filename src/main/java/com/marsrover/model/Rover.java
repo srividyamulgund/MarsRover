045e3e45c14e01d6ec88f7399d304ff357df4479
Srividya Mulgund <srividya.mulgund@gmail.com>
2021-05-18 11:44:38 +0100

    Add Rover test and implementation

--- src/main/java/com/marsrover/model/Rover.java
+++ src/main/java/com/marsrover/model/Rover.java
@@ -2,4 +2,37 @@ package com.marsrover.model;
 
 public class Rover {
 
+    private int X;
+    private int Y;
+    private char orientation;
+
+    public Rover(int x, int y, char orientation) {
+        this.X = x;
+        this.Y = y;
+        this.orientation = orientation;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
+
+    public char getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(char orientation) {
+        this.orientation = orientation;
+    }
 }
--- src/test/java/com/marsrover/model/RoverTest.java
+++ src/test/java/com/marsrover/model/RoverTest.java
@@ -2,6 +2,24 @@ package com.marsrover.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
 class RoverTest {
+
+    @Test
+    @DisplayName("returns Rover Orientation for a Rover object")
+    public void testRoverOrientation(){
+        Rover r1 = new Rover(1,2,'N');
+        assertEquals('N', r1.getOrientation(), "Orientation is correct");
+    }
+
+    @Test
+    @DisplayName("returns Rover co-ordinates for a Rover object")
+    public void testRoverPoints() {
+        Rover r1 = new Rover(1,2,'N');
+        assertEquals(1, r1.getX(), "X co-ordinate is correct");
+        assertEquals(2,r1.getY(), "Y co-ordinate is correct");
+    }
 
 }
