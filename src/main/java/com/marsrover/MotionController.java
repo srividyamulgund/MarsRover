b3dd88a30390d3cb7bf29100cce690082ca62d1f
Srividya Mulgund <srividya.mulgund@gmail.com>
2021-05-18 12:32:39 +0100

    Added MotionController Rover Position InBounds test and implementation

--- src/main/java/com/marsrover/MotionController.java
+++ src/main/java/com/marsrover/MotionController.java
@@ -1,4 +1,20 @@
 package com.marsrover;
 
+import com.marsrover.model.Plateau;
+import com.marsrover.model.Rover;
+
 public class MotionController {
+    Plateau plateau;
+    Rover rover;
+
+    public MotionController(Plateau plateau, Rover rover) {
+        this.plateau = plateau;
+        this.rover = rover;
+    }
+
+    public boolean isRoverPositionInBounds(Plateau plateau, Rover rover) {
+
+        return (rover.getX() <= plateau.getX()) && (rover.getX() >= 0 && rover.getY() >= 0)
+                && (rover.getY() <= plateau.getY());
+    }
 }
--- src/test/java/com/marsrover/MotionControllerTest.java
+++ src/test/java/com/marsrover/MotionControllerTest.java
@@ -1,7 +1,21 @@
 package com.marsrover;
 
+import com.marsrover.model.Plateau;
+import com.marsrover.model.Rover;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MotionControllerTest {
 
+    @Test
+    @DisplayName("returns false if Rover is outside the Plateau")
+    public void testRover1Position() {
+        Plateau plateau = new Plateau(5, 5);
+        Rover rover1 = new Rover(1, 2, 'N');
+        MotionController motionControllerRover1 = new MotionController(plateau, rover1);
+        Assertions.assertTrue(motionControllerRover1.isRoverPositionInBounds(plateau, rover1), "Rover cannot be out of the Plateau");
+    }
 }
