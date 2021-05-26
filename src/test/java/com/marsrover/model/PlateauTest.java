beba48357ba6abf85464703cc7471ce1eb14708c
Srividya Mulgund <srividya.mulgund@gmail.com>
2021-05-26 17:18:41 +0100

    Tests for Placing Rovers implemented

--- src/test/java/com/marsrover/model/PlateauTest.java
+++ src/test/java/com/marsrover/model/PlateauTest.java
@@ -48,15 +48,31 @@ class PlateauTest {
 
     @Test
     @DisplayName("returns top right co-ordinates of the plateau")
-    public static void testPlaceRovers(){
+    public void testPlaceRovers(){
         Rover rover1 = new Rover('1', '2', "Rover1", 'N');
         Rover rover2 = new Rover('3', '3', "Rover2", 'W');
         Plateau pl1 = new Plateau(5,5);
 
+        boolean rover1Flag = false;
+        boolean rover2Flag = false;
+
        Plateau.placeRovers(rover1, rover2);
-
-        //assertEquals(, "Cannot place rovers");
+       Map<String, Point> actualPlacement = Plateau.getRoverMap();
+        for(String key : actualPlacement.keySet()){
+            if(!key.equals(rover2.getName())){
+                Point position1 = actualPlacement.get(key);
+                if(position1.x == rover1.getX() && position1.y == rover1.getY()){
+                    rover1Flag = true;
+                }
+            }
+            else{
+                Point position2 = actualPlacement.get(key);
+                if(position2.x == rover2.getX() && position2.y == rover2.getY()){
+                    rover2Flag = true;
+                }
+            }
+        }
+        assertTrue(rover1Flag, "Rover 1 was npt placed on the Plateau");
+        assertTrue(rover2Flag, "Rover 2 was npt placed on the Plateau");
     }
-
-
 }
